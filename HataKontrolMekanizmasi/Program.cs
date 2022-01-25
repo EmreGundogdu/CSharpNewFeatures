ba2eeb8f72b18aae41897659bbc75fcf5bfe9175
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-25 16:37:34 +0300

    try catch

--- HataKontrolMekanizmasi/Program.cs
+++ HataKontrolMekanizmasi/Program.cs
@@ -36,18 +36,23 @@ namespace HataKontrolMekanizmasi
                 //hataya dair; log, kullanıcı bilgilendirme, kontrlü kapanış vs.
             }
             #endregion
+            Console.Write("Lütfen Birinci Sayıyı Giriniz");
+            int sayi1 = 0, sayi2 = 0;
             try
-            {
-                Console.Write("Lütfen Birinci Sayıyı Giriniz");
-                int sayi1 = int.Parse(Console.ReadLine());
+            {                
+                sayi1 = int.Parse(Console.ReadLine());
                 Console.WriteLine("Lütfen İkinci Sayıyı Giriniz");
-                int sayi2 = int.Parse(Console.ReadLine());
+                sayi2 = int.Parse(Console.ReadLine());
                 Console.WriteLine("Toplam : " + (sayi1 + sayi2));
             }
             catch
             {
-
+                Console.WriteLine("Lütfen doğru bir ifade giriniz");
             }
+
+            #endregion
+            #region  Try Catch Hata Parametreleri
+            //Çalışma zamanında alınan hataya dair bizlere bilgi veren/taşıyan parametredir.
 
             #endregion
         }
