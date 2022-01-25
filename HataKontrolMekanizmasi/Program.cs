449daecd779dad6190c5fd0c4d261603d0f8a705
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-25 16:06:56 +0300

    Try Catch

---
+++ HataKontrolMekanizmasi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HataKontrolMekanizmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Hata Kontrol Mekanizmları
+            //3 tür hata tipi vardır
+            //1. Derleyici/Sözdizi hataları
+            //2. Runtime hataları
+            //3. Mantıksal hatalar
+
+            //Syntax Error
+            /*
+             * Dil kurallarına aykırı olan hatalar
+             */
+            #endregion
+            #region Çalışma Zamanı Hata Durumaları Örnek
+            //Console.Write("Lütfen Birinci Sayıyı Giriniz");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Lütfen İkinci Sayıyı Giriniz");
+            //int sayi2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Toplam : " + (sayi1 + sayi2));
+            #endregion
+            #region Try Catch
+            #region Try Catch Iskelet Yapısı
+            try
+            {
+                //Olası çalışma zamanı hatarlını barındıran/verebilecek olan kodları buraya yazıyoruz
+            }
+            catch
+            {
+                //try içeerisinde bir hata söz konusu olduğunda catch bloğu tetiklenecektir.
+                //hataya dair; log, kullanıcı bilgilendirme, kontrlü kapanış vs.
+            }
+            #endregion
+            try
+            {
+                Console.Write("Lütfen Birinci Sayıyı Giriniz");
+                int sayi1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Lütfen İkinci Sayıyı Giriniz");
+                int sayi2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Toplam : " + (sayi1 + sayi2));
+            }
+            catch
+            {
+
+            }
+
+            #endregion
+        }
+    }
+}
