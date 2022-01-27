0fb952ad7364d2976cedf9ded84a740eab7744ee
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-27 16:47:50 +0300

    Keywords

---
+++ Keywords/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Keyword : Proghramlama dilinin en atomik parçalarıdır diyebiliriz
+
+        }
+    }
+}
