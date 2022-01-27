9fec103a0e9b7e69afd284a0e272ff49e05cc788
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-27 19:15:48 +0300

    break komutu

--- Keywords/Program.cs
+++ Keywords/Program.cs
@@ -6,7 +6,49 @@ namespace Keywords
     {
         static void Main(string[] args)
         {
-            //Keyword : Proghramlama dilinin en atomik parçalarıdır diyebiliriz
+            //Keyword : Programlama dilinin en atomik parçalarıdır diyebiliriz
+            #region Yardımcı Manevratik Komutlar
+            #region Break
+            //Switch-case ve döngülerde kullanılabilir bi keyworddür. Kullanıldığı yapıdan çıkış yapmayı. Sonlandırmayı yarayan keyworddür.
+            //switch (10)
+            //{
+            //    case 5:
+            //        break;
+            //    case 10:
+            //        break;
+            //    case 15:
+            //        break;
+            //}
+            //while (true)
+            //{
+            //    break;
+            //}
+            //for (int i = 0; i < length; i++)
+            //{
+            //    break;
+            //}
+            //foreach (var item in collection)
+            //{
+            //    break;
+            //}
+            //for (int i = 0; i < length; i++)
+            //{
+            //    while (true)
+            //    {
+            //        break;
+            //    }
+            //    break;
+            //}
+            do
+            {
+                if (true)
+                {
+                    break;
+                }
+            } while (true);
+            //TRY CATCH BLOĞUNDA BREAK KULLANILAMAZ ANCAK BİR DONGU ICERISINDE TANIMLANAN TRY CATCH BLOGUNDA KULLANILABİLİR
+            #endregion
+            #endregion
 
         }
     }
