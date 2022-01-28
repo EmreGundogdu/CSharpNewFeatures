7cdb8d99bffd087b0386815819bac0c0ca4802d3
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-28 23:04:44 +0300

    dizi varyasyonlari

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -42,11 +42,39 @@ namespace Diziler
             {
                 //Console.WriteLine(personeller[x++]);
             } while (x < 10);
-
+            #region Diziyi yazdıran döngü
             for (int i = 0; i < personeller.Length; i++)
             {
                 Console.WriteLine(personeller[i]);
             }
+            #endregion
+
+            #endregion
+            #region Sınırlılıklar
+            //Dizilerde tanımlama yaparken eleman sayısının bildiirlmesi zorunluluğu bir sınırlılıktır
+            //Diziler tanımlandığında kullansakta kullanmasakta bellekte belirtilen eleman sayısı kadar alan tahsisinde bulunurlar. Bu durumda bellek boyunda ekstradan maliyete sebep olacağı için bir sınırlılıktır.
+            //Dizilerde eleman sayısının başta belirlenmesi durumunda, ihtiyaca binaen daha fazla değer atamak istediğimizde bu değerleri atayamacağımızdan ve dizinin aralığını genişletemeyeceğimizden dolayı bu durum bir sınırlılık olarak karşımıza çıkmaktadır.
+            //Dizilerde elemanlara değer atarken indexeer operatörüyle çok haşır neşir olunmaktadır. Bu durumda bir sınırlılıktır.
+            #endregion
+            #region Dizi Tanımlama Varyasyonları
+            #region 1. Varyasyon
+            int[] degerler = new int[5];
+            degerler[0] = 1;
+            Console.WriteLine(degerler[0]);
+            #endregion
+            #region 2. Varyasyon
+            int[] degerler2 = { 10, 20, 21, 22, 23 };
+            string[] isimler = { "Emre", "Fatih", "Serhat" };
+            #endregion
+            #region 3. Varyasyon
+            string[] isimler2 = new string[] { "Emre", "Fatih", "Serhat" };
+            #endregion
+            #region 4. Varyasyon
+            string[] isimler3 = new string[3] { "", "", "" };//belirtilen(3) kadar süslü parantez içerisine değer girmek gerekir.
+            #endregion
+            #region 5. Varyasyon
+
+            #endregion
             #endregion
         }
     }
