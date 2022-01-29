88498cbb6a9f234278c0abca1fe875a1249fd485
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-01-29 17:31:27 +0300

    Array copy metodu

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -33,20 +33,20 @@ namespace Diziler
             personeller[8] = "İdil";
             personeller[9] = "İrem";
             //Diziler genellikle döngülerle birlikte algoritmalarda kullanılmaktadırlar/işlenmektedirler
-            for (int i = 0; i < 10; i++)
-            {
-                //Console.WriteLine(personeller[i]);
-            }
-            int x = 0;
-            do
-            {
-                //Console.WriteLine(personeller[x++]);
-            } while (x < 10);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(personeller[i]);
+            //}
+            //int x = 0;
+            //do
+            //{
+            //    Console.WriteLine(personeller[x++]);
+            //} while (x < 10);
             #region Diziyi yazdıran döngü
-            for (int i = 0; i < personeller.Length; i++)
-            {
-                Console.WriteLine(personeller[i]);
-            }
+            //for (int i = 0; i < personeller.Length; i++)
+            //{
+            //    Console.WriteLine(personeller[i]);
+            //}
             #endregion
 
             #endregion
@@ -58,9 +58,9 @@ namespace Diziler
             #endregion
             #region Dizi Tanımlama Varyasyonları
             #region 1. Varyasyon
-            int[] degerler = new int[5];
-            degerler[0] = 1;
-            Console.WriteLine(degerler[0]);
+            //int[] degerler = new int[5];
+            //degerler[0] = 1;
+            //Console.WriteLine(degerler[0]);
             #endregion
             #region 2. Varyasyon
             int[] degerler2 = { 10, 20, 21, 22, 23 };
@@ -81,11 +81,59 @@ namespace Diziler
             #region Array Sınıfı
 
             Array yaslar2 = new int[3]; //Array = Dizi | Array yerine int[] yazabilirdik
+
             //Dizi olarak tanımlanan değişkenler Array sınıfından türetilmektedirler.
             //Dizilerde Array sınıfıdan gelen berlirli metotlar ve özellikler mevcuttur
 
-            int[] a = new int[5];//kendi türünde tutuyorsak indexer operatörü kullanılabilir.
-            Array a2 = new int[5];//Array türünde tutuyorsak indexer operatörü kullanılamaz
+            int[] a = new int[5];//kendi türünde tutuyorsak indexer operatörü kullanılabilir. bu şeklde açlışıldığında ilgili diziye verisel müdahaleler operatif gerçekleştirilir. Bu format algoritmalarda tercih edilir indexer kullanılabilir olduğundan dolayı.
+            Array a2 = new int[5];//Array türünde tutuyorsak indexer operatörü kullanılamaz. Bu şekilde çalışılırken ise fonksiyonel çözümler getirilmektedir.
+
+
+
+            #endregion
+            #region Array Türünden Dizilere Değer Atama / Okuma
+            #region 1. Yöntem
+            int[] dizi = new int[2];
+            dizi[0] = 20;
+            dizi[1] = 10;
+            Array dizi2 = dizi;
+            #endregion
+            #region 2. Yöntem
+            //Array dizi3 = new int[] { 1, 2, 3, 4, 5 };
+            //Array dizi4 = new int[2] { 1, 2 };
+            Array dizi5 = new[] { 1, 2, 3, 4 };
+            #endregion
+            #region 3. Yöntem
+            Array dizi7 = new int[3];
+            dizi7.SetValue(5, 0);
+            dizi7.SetValue(10, 1);
+            dizi7.SetValue(15, 2);
+            #endregion
+            #region Okuma
+            object value = dizi.GetValue(1);//object olarak değeri verir
+            #endregion
+            #endregion
+            #region Array Sınıfı Clear Metodu
+            //Dizi içerisindeki tüm elemanları siliyor diye bilinir. Halbuki bu yanlıştır. Dizi içerisindeki tüm elemanlara, dizinin türüne uygun default değerleri atayan bir fonksiyondur
+
+            Array isimler4 = new[] { "emre", "fatih", "serhat", "yaren" };
+            for (int i = 0; i < isimler4.Length; i++)
+                Console.WriteLine(isimler4.GetValue(i));
+            Array.Clear(isimler4, 0, isimler4.Length); //temizlenecek array ismi | hangi indexten başlıcak? | nereye kadar silicek
+            Console.WriteLine("********"); //Clear sonrası
+            for (int i = 0; i < isimler4.Length; i++)
+                Console.WriteLine(isimler4.GetValue(i));
+            #endregion
+            #region Array Sınıfı Copy Metodu
+            Console.WriteLine("***COPY***");
+            //Elimizdeki bir dizinin verilerini bir başka diziye kopyalamamızı sağlayan bir fonksiyondur
+            Array isimler5 = new[] { "emre", "fatih", "serhat", "yaren" };
+            string[] isimler6 = new string[isimler5.Length];
+            Array.Copy(isimler5, isimler6, 4);
+            for (int i = 0; i < isimler6.Length; i++)
+            {
+                Console.WriteLine(isimler6[i]);
+            }
             #endregion
         }
     }
