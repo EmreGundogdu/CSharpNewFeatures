a4346e6ec9a213f7b8e70c653bd0ece26f4924c9
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-01 21:45:32 +0300

    ArrayList

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+
+            #endregion
+            #region ArrayList Koleksiyonu Tanımlama
+            //Diziler tanımlanırken kaç eleman tutacakları bildirilmek zorunludur.
+            int[] yaslar = new int[15];
+            ArrayList _yaslar = new ArrayList();
+            #endregion
+            #region Tanımlanmış Koleksiyona Değer Atama
+            _yaslar.Add(21);
+            #endregion
+        }
+    }
+}
