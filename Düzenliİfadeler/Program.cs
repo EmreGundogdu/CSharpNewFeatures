74e9788b1c89fa9793c24bf86f2b14a417221e9e
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-01 16:24:53 +0300

    Regular Expressions

---
+++ Düzenliİfadeler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Düzenliİfadeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Regular Expressions Operators
+            //^ Operatörü : Satır başının istenilen ifadeyle başlamasını sağlar.
+            //^9 : 99dasdas, 9dsada, 9-*-/2132 ||| dsa999, *-*-**9,
+            string text = "9414857312218763";
+            Regex regex = new Regex("^9");
+            Match match = regex.Match(text);
+            Console.WriteLine(match.Success);
+            #endregion
+        }
+    }
+}
