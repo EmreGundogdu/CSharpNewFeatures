a4fe487bc9a0bc7954786934ae058a57a0941cdb
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-01 12:27:48 +0300

    ArraySegment

--- 12.DizilerdeVeriselPerformans/Program.cs
+++ 12.DizilerdeVeriselPerformans/Program.cs
@@ -14,6 +14,9 @@ namespace DizilerdeVeriselPerformans
             sayilar2[3] *= 10;
             sayilar2[4] *= 10;
             string text = "Hallo, wir sind da";
+            #region ArraySegment Nedir?
+            //Bir dizinin bütününden ziyade belirli bir kısmına yahut parçasına ihtiyaç dahilinde ilgili diziyi kopyalamak yerine(ki görece oldukça maliyetli bir operasyondur) bağımsız bir referans ile erişmemizi ve böylece salt bir şekilde temsil etmemizi sağlayan bir yapıdır.
+            #endregion
         }
     }
 }
