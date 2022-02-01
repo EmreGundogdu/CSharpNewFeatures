024780989aa53abfab6fef6ff98890a706e5d19a
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-01 12:35:11 +0300

    ArraySegment Türü İle Dizinin Belli Bir Alanını Referans Etmek

--- 12.DizilerdeVeriselPerformans/Program.cs
+++ 12.DizilerdeVeriselPerformans/Program.cs
@@ -7,15 +7,21 @@ namespace DizilerdeVeriselPerformans
         static void Main(string[] args)
         {
             int[] sayilar = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            int[] sayilar2 = sayilar[2..7];
-            sayilar2[0] *= 10;
-            sayilar2[1] *= 10;
-            sayilar2[2] *= 10;
-            sayilar2[3] *= 10;
-            sayilar2[4] *= 10;
-            string text = "Hallo, wir sind da";
+            //int[] sayilar2 = sayilar[2..7];
+            //sayilar2[0] *= 10;
+            //sayilar2[1] *= 10;
+            //sayilar2[2] *= 10;
+            //sayilar2[3] *= 10;
+            //sayilar2[4] *= 10;
+            //string text = "Hallo, wir sind da";
             #region ArraySegment Nedir?
             //Bir dizinin bütününden ziyade belirli bir kısmına yahut parçasına ihtiyaç dahilinde ilgili diziyi kopyalamak yerine(ki görece oldukça maliyetli bir operasyondur) bağımsız bir referans ile erişmemizi ve böylece salt bir şekilde temsil etmemizi sağlayan bir yapıdır.
+            #endregion
+            #region ArraySegment İle Dizinin Belli Bir Alanını Referans Etmek
+            ArraySegment<int> segment1 = new ArraySegment<int>(sayilar); //Dizinin tüm elemanlarını temsile eder.
+            ArraySegment<int> segment2 = new ArraySegment<int>(sayilar, 2, 5); //2. indexten başla 5.'ye kadar(5 sayarken normal 1'den başladık) | 30-50
+            segment1[0] *= 10;
+            segment2[0] *= 10;
             #endregion
         }
     }
