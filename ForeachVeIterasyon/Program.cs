2493ee558726419024c6166cd4fc0292bde4672f
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-02 10:42:55 +0300

    Iterasyon

---
+++ ForeachVeIterasyon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForeachVeIterasyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Iterasyon - Döngü
+            //Foreach(Iterasyon) BİR DÖNGÜ DEĞİLDİR
+            int[] sayilar = { 3, 123, 42, 63, 53, 123, 51, 23 };
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+
+            }
+            #endregion
+        }
+    }
+}
