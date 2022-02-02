9e037cce0827a1c590a5c6e04b66dbcbdd6e00b5
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-02 22:43:55 +0300

    parapmetre alan geriye değer döndüren metotlar

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -70,7 +70,14 @@ namespace Metotlar
         }
         #endregion
         #region Geriye Değer Döndüren, Parametre Alan Metot
-
+        public bool Metot7(int x)
+        {
+            return true || false;
+        }
+        public int Metot8(int x)
+        {
+            return DateTime.Now.Year > 2000 ? 1 : 0;
+        }
         #endregion
     }
 }
