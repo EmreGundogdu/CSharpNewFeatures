8157952e346a7e1290036337a19495f69682b273
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-02 20:50:34 +0300

    Metotlar

---
+++ Metotlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = new Random().Next();
+            }
+            foreach (var sayi in sayilar)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
