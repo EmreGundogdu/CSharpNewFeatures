035864af450590aa379f08dd2d2ae743b97d08e1
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-02 22:40:10 +0300

    geriye değer döndüren metotlar

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -55,9 +55,18 @@ namespace Metotlar
         }
         #endregion
         #region Geriye Değer Döndüren, Parametre Almayan Metot
+        //Geriye değer döndüren fonksiyonlarda, bildirilen türde bir değer döndürebilmek için return keywordü kullanmamız gerekir.
+        //return : nerede tetiklenir işlenirse orada ilgili fonksiyondan/metotdan çıkılır.
         private char Metot5()
         {
+            return 'a';
+        }
+        private int Metot6()
+        {
+            if (DateTime.Now.Second > 10) //bu şekilde hata verir çünkü if'in false olma durumunda ne yapacağını bilmez.
+                return 5;
 
+            return 10;  // -> false durumdaki durumu eklemiz gerekir.
         }
         #endregion
         #region Geriye Değer Döndüren, Parametre Alan Metot
