e0b677bf03914f3f8960341305b6db5e17bd2337
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-02 22:25:42 +0300

    metotlar

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -6,15 +6,51 @@ namespace Metotlar
     {
         static void Main(string[] args)
         {
-            int[] sayilar = new int[10];
-            for (int i = 0; i < sayilar.Length; i++)
-            {
-                sayilar[i] = new Random().Next();
-            }
-            foreach (var sayi in sayilar)
-            {
-                Console.WriteLine(sayi);
-            }
+            #region Örnek döngü
+            //int[] sayilar = new int[10];
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    sayilar[i] = new Random().Next();
+            //}
+            //foreach (var sayi in sayilar)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+            #endregion
+            //Metotlar class içerisinde oluşturulmalıdır.
+
+            //Metot Oluşturma
+            //erişim belirleyicisi   geri dönüş değeri   metodun adı()
+            //{
+            //}
+
+
+
+
         }
+
+        //erişim belirleyici   geri dönüş değeri   metot adı()
+
+        //Dört Farklı Türde metot oluşturma;
+
+        #region Geriye Değer Döndürmeyen, Parametre Almayan Metot
+        private void Metot1()
+        {
+            //döngüler vs yapılabilir.
+            Console.WriteLine("Geriye Değer Döndürmeyen, Parametre Almayan Metot Oluşturuldu");
+        }
+        #endregion
+        #region Geriye Değer Döndürmeye, Parametre Alan Metot
+        private void Metot2(int a)
+        {
+
+        }
+        #endregion
+        #region Geriye Değer Döndüren, Parametre Almayan Metot
+
+        #endregion
+        #region Geriye Değer Döndüren, Parametre Alan Metot
+
+        #endregion
     }
 }
